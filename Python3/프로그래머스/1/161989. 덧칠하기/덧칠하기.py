ada7ded2149bfc58cdbb8d16685a93bea4b06c6a
Suyeon Woo <125536266+igotyabingo@users.noreply.github.com>
2025-02-03 20:31:02 +0900

    Update 덧칠하기.py

--- Python3/프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ Python3/프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -2,7 +2,6 @@ def solution(n, m, section):
     answer = 0
     pos = section[0]
     
-    # set에 남아있는 원소가 없을 때 까지
     for i in section[1:]:
         if(pos+m <= i):
             # update
@@ -10,3 +9,5 @@ def solution(n, m, section):
             answer += 1
             
     return answer+1
+
+# 처음에는 set의 차집합 연산을 이용하여 풀었으나 len(section)**2의 시간이 소요되면 몇가지 테케에서 시간이 초과됨
