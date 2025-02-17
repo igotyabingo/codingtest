68bdb7c43021c50213bf10098b9a4d4d2e7ae912
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-02-17 09:25:33 +0900

    Update 최댓값과 최솟값.py

--- Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,6 @@
 def solution(s):
     lst = [int(i) for i in s.split()]
+    # map 함수를 응용해볼 수 있다: s.split()에 int함수를 매핑
+    # list = list(map(int, s.split()))
+    
     return f'{min(lst)} {max(lst)}'
