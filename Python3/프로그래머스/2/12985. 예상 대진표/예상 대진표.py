3e0bf8969d481ef164290d6c1e849dd11901109c
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-02-23 10:01:02 +0900

    no need to branch odd/even

--- Python3/프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ Python3/프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -12,3 +12,10 @@ def next_round(i):
         return i//2
     else:
         return (i+1)//2
+
+'''
+while a != b:
+    answer += 1
+    a, b = (a+1)//2, (b+1)//2    
+와 같이 작성하면 1,2 vs 2,3 처리도 되고 짝수 홀수 나눠서 다음 라운드 번호를 구핦 필요도 없다.
+'''
