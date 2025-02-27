77d3d6d4cf9e737b3ca7775b4ae232771dcdc885
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-02-27 10:14:00 +0900

    [level 2] Title: [1차] 캐시, Time: 106.99 ms, Memory: 17.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,21 @@
+def solution(cacheSize, cities):
+    answer = 0
+
+    if(cacheSize == 0):
+        return 5*len(cities)
+
+    for i, j in enumerate(cities):
+          cities[i] = j.lower()
+
+    cache = []
+    
+    for i in cities:
+        if i in cache:
+            answer += 1
+            cache.pop(cache.index(i))
+        else:
+            answer += 5
+            if len(cache) == cacheSize: cache.pop(0)
+        cache.append(i)
+    
+    return answer
