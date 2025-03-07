5bf5475f06f8cb6f512db559d18fe661030f561a
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-03-07 09:01:16 +0900

    [level 2] Title: [3차] 압축, Time: 0.80 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ Python3/프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -6,22 +6,17 @@ def solution(msg):
     for i in range(26):
         dictionary[chr(65+i)] = i+1
     
-    while(msg):
-        # 2. 현재 입력과 일치하는 가장 긴 문자열을 찾는다.
-        val = False
-        for i in range(1, len(msg)+1):
-            if msg[:i] not in dictionary:
-                val = True
-                break
-        if i>1 and val:
-             answer.append(dictionary[msg[:i-1]])      
+    while True:
+        # 마지막 집합 처리
+        if msg in dictionary:
+            answer.append(dictionary[msg])
+            break
         else:
-             answer.append(dictionary[msg])
-        
-        if(val):
-             dictionary[msg[:i]] = len(dictionary)+1
-             msg = msg[i-1:]
-        else:
-             break
+            for i in range(1, len(msg)+1):
+                if msg[0:i] not in dictionary:  # 0부터 슬라이싱하여 i-1 범위 처리를 하지 않아도 되게 한다
+                    answer.append(dictionary[msg[0:i-1]])
+                    dictionary[msg[0:i]] = len(dictionary)+1
+                    msg = msg[i-1:]
+                    break
    
     return answer
