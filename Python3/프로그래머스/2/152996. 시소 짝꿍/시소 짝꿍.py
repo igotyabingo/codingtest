82b560b8fd1b011edca138536aafeb37f1924eb6
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-03-23 16:03:55 +0900

    [level 2] Title: 시소 짝꿍, Time: 74.75 ms, Memory: 9.71 MB -BaekjoonHub

--- Python3/프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ Python3/프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -3,7 +3,6 @@ from collections import Counter
 def solution(weights):
     count = 0
     
-    weights.sort()
     dic = dict(Counter(weights))
     weights = list(dic.keys())
 
@@ -14,7 +13,7 @@ def solution(weights):
         del dic[target]
 
         for other in dic:
-            if other/target in [1.5, 2.0, 4/3]:
+            if other/target in [1.5, 2.0, 4/3, 0.5, 2/3, 3/4]:
                 count += target_num*(dic[other])
 
     return count
