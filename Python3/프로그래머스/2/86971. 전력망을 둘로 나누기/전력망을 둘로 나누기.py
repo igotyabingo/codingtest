97ffa58530741f2795a07bf39887a1eace3ec31f
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-04-05 10:01:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.00 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,22 +1,37 @@
+# 전선 하나씩을 끊어보면서 BFS로 그래프를 생성한다.
+from collections import deque
+
+def bfs(start, graph, visited):
+    q = deque([start])
+    visited[start] = True
+    count = 1
+    
+    while q:
+        now = q.popleft()
+        for nxt in graph[now]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                q.append(nxt)
+                count += 1
+    
+    return count
+
 def solution(n, wires):
-    answer = 1000
+    answer = n
 
-    for i in range(len(wires)):
-        target = wires[:i]+wires[i+1:]
-
-        all = set([j for j in range(1, n+1)])
-        group = set(target[0])
-
-        for _ in range(n):
-            for w in target:
-                a, b = w
-                if a in group or b in group:
-                    group.add(a)
-                    group.add(b)
-
-        a, b = len(all-group), len(group)
-        tmp = max(a-b, b-a)
-        answer = min(answer, tmp)
-        # print(all-group, group, answer)
+    for i in range(len(wires)): # 끊을 전선 wires[i]
+        graph = [[] for _ in range(n+1)]
+        for j in range(len(wires)):
+            if i == j:
+                continue
+            a, b = wires[j]
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        visited = [False] * (n+1)
+        group_size = bfs(1, graph, visited)
+        other_size = n - group_size
+        
+        answer = min(answer, abs(group_size-other_size))
 
     return answer
