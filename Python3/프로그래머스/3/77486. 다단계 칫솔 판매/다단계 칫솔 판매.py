4c0a844795a40e8f015758f3de70875834edc715
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-06-03 16:00:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 118.05 ms, Memory: 19.9 MB -BaekjoonHub

--- Python3/프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ Python3/프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -4,26 +4,27 @@ def solution(enroll, referral, seller, amount):
     n = len(enroll)
     answer = [0 for _ in range(n)]
     name = dict()
-    parent = dict() # 인덱스: [부모 인덱스들] 형태로 저장
+    parent = [-1 for _ in range(n)] # 인덱스: [부모 인덱스들] 형태로 저장
     
     # name, parent 딕셔너리 초기화
     for i in range(n):
         name[enroll[i]] = i
         if referral[i] != "-":
-            parent[i] = [name[referral[i]]] + parent[name[referral[i]]]
-        else:
-            parent[i] = []
+            parent[i] = name[referral[i]]
     
     # seller 순환하며 분배, 계산
     for i, s in enumerate(seller):
-        # name[s] = seller's index, amount*100 = 판매액
-        price = amount[i]*100
-        
-        for p in [name[s]]+parent[name[s]]:
+        target = name[s]
+        price = amount[i] * 100
+        while parent[target] != -1:
             val = price // 10
-            answer[p] += price - val
+            answer[target] += price - val
+            
+            target = parent[target]
+            price = val
             if val == 0:
                 break
-            price = val
+                
+        answer[target] += price - price // 10
         
     return answer
