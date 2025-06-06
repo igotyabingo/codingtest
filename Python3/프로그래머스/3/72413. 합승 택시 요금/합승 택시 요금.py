eade36297b0713b8cd1af3d5f17aa863f4a735d0
Suyeon Woo <jenifer7933@snu.ac.kr>
2025-06-06 22:32:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 4033.21 ms, Memory: 16.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ Python3/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,20 +1,29 @@
+import heapq
+from collections import defaultdict
+
 def solution(n, s, a, b, fares):
-    # 1. 각 지점에서 각 지점 까지의 최단거리를 계산: floyd-warshall 알고리즘
-    dist = [[float('inf')] * n for _ in range(n)]
-    for i in range(n):
-        dist[i][i] = 0
-    
-    for i, j, d in fares:
-        dist[i-1][j-1] = d
-        dist[j-1][i-1] = d
-    
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
-    
     answer = float('inf')
-    for k in range(n):
-        answer = min(answer, dist[s-1][k]+dist[k][a-1]+dist[k][b-1])    
+    # 1. 각 지점에서 각 지점 까지의 최단거리를 계산: 각 지점에 대해 다익스트라 알고리즘을 사용
+    graph = defaultdict(list)
+    for i, j, d in fares:
+        graph[i].append((j, d))
+        graph[j].append((i, d))
     
+    distance = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+    for i in range(1, n+1):
+        distance[i][i] = 0
+        queue = []
+        heapq.heappush(queue, (i, 0))
+        
+        while queue:
+            node, d = heapq.heappop(queue)
+            if distance[i][node] >= d:
+                for neighbor, cost in graph[node]:
+                    if distance[i][neighbor] > cost + d:
+                        distance[i][neighbor] = cost + d
+                        heapq.heappush(queue, (neighbor, cost+d))
+    
+    for i in range(1, n+1):
+        answer = min(answer, distance[s][i]+distance[i][a]+distance[i][b])
+   
     return answer
