9ef319b1bfdcd6b8dc16a35df0dd30820129aa46
igotyabingo <jenifer7933@snu.ac.kr>
2025-11-13 18:14:02 +0900

    [level 2] Title: 프로세스, Time: 0.14 ms, Memory: 9.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/42587. 프로세스/프로세스.py
+++ Python3/프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,34 +1,27 @@
+from collections import defaultdict, deque
+
 def solution(priorities, location):
-    queue = []  # 프로세스에 이름 붙이기
-    dic = dict()    # 프로세스별 우선순위 딕셔너리로 정리
-    target = chr(65+location)   # 순서 찾는 프로세스 알파벳 저장
-    p = dict()  # 우선순위별 잔여 프로세스 개수 딕셔너리로 정리
+    # 우선순위 별 process 개수 저장
+    p = defaultdict(int)
+    queue = []
     
-    for i in priorities:
-        if i in p:
-            p[i] += 1
-        else:
-            p[i] = 1
-    
-    for i in range(len(priorities)):
-        queue.append(chr(65+i))  
-        dic[chr(65+i)] = priorities[i]
-    answer = 0
-    
-    while(True):
-        answer += 1
-        mx = max(p)        
+    for i, pr in enumerate(priorities):
+        p[pr] += 1
+        queue.append((i, pr))
         
-        while(True):
-            k = queue[0]
-            
-            if dic[k] == mx:
-                queue.pop(0)
-                p[mx] -= 1
-                if p[mx] == 0:
-                    del p[mx]
-                break
-            else:
-                queue.append(queue.pop(0))
-        if(k == target):
-            return answer
+    queue = deque(queue)
+    mx = max(p)
+    rank = 1
+    
+    while queue:
+        x, y = queue.popleft()
+        if y < mx:
+            queue.append((x, y))
+        else: # y = mx
+            if location == x:
+                return rank
+            p[mx] -= 1
+            if p[mx] == 0:
+                del p[mx]
+                mx = max(p)
+            rank += 1
