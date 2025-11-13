054d7d24cf109d6401a1c58ce5adee0bd79d01cd
igotyabingo <jenifer7933@snu.ac.kr>
2025-11-13 16:09:58 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,8 @@
 def solution(nums):
-    # nums의 distinct한 종류 개수가 N/2보다 클 경우 N/2를 리턴,
-    # 그렇지 않을 경우 distinct한 종류 개수를 리턴
+    # 번호 = 종류
+    # 전체 N마리 중 N/2마리를 가져갈 수 있다 -> N/2마리를 가져가는 것과 N/2 종류를 가져가는 것은 완전 별개임
+    # 최대한 다양한 종류(리턴값)를 보장하면서 N/2마리 선택
     
-    # distinct = 'set'
-
-    return min(len(nums)//2, len(set(nums)))
+    # nums에서 distinct한 원소의 개수와 n중의 min값이 리턴값이 됨
+    
+    return min(len(set(nums)), len(nums)//2)
