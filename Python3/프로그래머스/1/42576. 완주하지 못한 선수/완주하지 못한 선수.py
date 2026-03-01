0ed3646af59f482b54e50237a539698cb200b796
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-01 22:06:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 36.63 ms, Memory: 38.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,8 @@
+from collections import Counter
 def solution(participant, completion):
-    # set으로 변환 후 차집합 구하는 것 불가능: 동명이인이 있을 수 있음
-    # 각 이름 별로 count 확인
-    count = dict()
-    for p in participant:
-        if p in count: 
-            count[p] += 1
-        else:
-            count[p] = 1
+    p = Counter(participant)
+    c = Counter(completion)
     
-    for c in completion:
-        count[c] -= 1
-    
-    for c in count:
-        if count[c] == 1:
-            return c
+    for name in p:
+        if not c[name] or p[name]!=c[name]:
+            return name
