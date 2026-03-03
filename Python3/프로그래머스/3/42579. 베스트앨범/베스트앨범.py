bfc20b3ed5e3654c9e736d41bfc70fb47ec8db04
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-03 11:20:52 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.23 MB -BaekjoonHub

--- Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ Python3/프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,21 +1,23 @@
 def solution(genres, plays):
     answer = []
+    # 1. 장르 정렬: 장르에 속한 노래들의 전체 재생 횟수가 많은 순서대로
+    # 2. 장르 내 노래 정렬: 각 노래의 재생 횟수가 많은 순서대로
+    # -> 재생횟수가 동일할 경우, 고유 번호 순서대로
     
-    count = dict()
-    # count[classic] = {count합, [(수록곡, count)]}
-    # 1. count합 기준으로 장르 정렬
-    # 2. 수록곡 list 정렬
+    # 1. 장르별 총 재생횟수 구하기
+    # 2. 장르별 노래 번호, 재생횟수 정리하기
     
-    for i, genre in enumerate(genres):
-        if genre not in count:
-            count[genre] = [0, []]
-        count[genre][0] += plays[i]
-        count[genre][1].append((i, plays[i]))
+    lst = dict()
+    # genre = [총 재생횟수, [[노래번호, 재생횟수], [노래번호, 재생횟수], ... ]]
+    for i in range(len(genres)):
+        if genres[i] not in lst:
+            lst[genres[i]] = [0, []]
+        lst[genres[i]][0] += plays[i]
+        lst[genres[i]][1].append([i, plays[i]])
     
-    count = dict(sorted(count.items(), key=lambda x:x[1][0], reverse=True))
-    for lst in count.values():
-        pieces = sorted(lst[1], key=lambda x:(x[1], -x[0]), reverse=True)
-        for piece in pieces[:2]:
-            answer.append(piece[0])
-            
+    for g in sorted(lst, key = lambda x:lst[x][0], reverse=True):
+        target = sorted(lst[g][1], key=(lambda x:x[1]), reverse=True)
+        for element in target[:2]:
+            answer.append(element[0])
+        
     return answer
