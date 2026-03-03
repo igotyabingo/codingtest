4bff34c47109759677e8321c1cf6d32807c5366b
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-03 10:58:56 +0900

    [level 2] Title: 전화번호 목록, Time: 103.37 ms, Memory: 29.9 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,21 +1,8 @@
 def solution(phone_book):
-    # phone_book의 각 원소 = 번호, 한 번호가 다른 번호의 '접두어'인 경우가 없는지 bool값 리턴
-    
-    # # 1. 모든 순서쌍 확인 (O(n**2)) / 있으면 바로 False 리턴하게 -> 효율성 테스트 통과 X
-    # for i, phone in enumerate(phone_book):
-    #     for target in phone_book[i+1:]:
-    #         if len(phone) >= len(target) and phone[:len(target)] == target:
-    #             return False
-    #         if len(phone) < len(target) and target[:len(phone)] == phone:
-    #             return False
-    # return True
-                
-    # 2. 정렬 -> 앞에 원소만 확인하면 됨 -> O(n)
+    # 사전순 정렬하면 바로 옆 번호만 확인하면 됨
     phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
-        
-    return True
-        
     
+    return True
