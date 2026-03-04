056b6ef25398e8ff83ff3db650fcb0a826c5b67f
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-04 22:21:36 +0900

    [level 2] Title: 주식가격, Time: 61.79 ms, Memory: 25 MB -BaekjoonHub

--- Python3/프로그래머스/2/42584. 주식가격/주식가격.py
+++ Python3/프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,26 +1,22 @@
 from collections import deque
 
 def solution(prices):
+    # ****
     answer = [0 for _ in range(len(prices))]
-
-    t = 0 # 현재 시간
     queue = deque()
+    stack = []
     
     for i, price in enumerate(prices):
         queue.append((i, price))
     
-    # stack에 price 하나씩 push, top원소보다 더 작은 게 들어오면 pop
-    stack = []
-    # stack = [(t, price)]
+    t = 0
     while queue:
-        while stack and stack[-1][1] > queue[0][1]:
+        while stack and stack[-1][1] > queue[0][1]: # 가격이 떨어짐
             time, _ = stack.pop()
-            answer[time] = t-time
+            answer[time] = t - time
         stack.append(queue.popleft())
         t += 1
-        
-    while stack:
-        time, _ = stack.pop()
-        answer[time] = t-time-1
-        
+    
+    for a, b in stack:
+        answer[a] = t - a - 1
     return answer
