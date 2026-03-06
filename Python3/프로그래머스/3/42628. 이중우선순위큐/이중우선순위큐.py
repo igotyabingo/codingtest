fa5b3e3764a6e10cf94e92b8f904723337b81fd5
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-06 18:35:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 82.09 ms, Memory: 19.4 MB -BaekjoonHub

--- Python3/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ Python3/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,37 +1,45 @@
-from heapq import heapify, heappop, heappush
+from heapq import heappush, heapify, heappop
+from collections import deque, defaultdict
 
 def solution(operations):
-    visited = set()
-    minheap, maxheap = [], []
+    # 최솟값, 최댓값을 모두 기록하고 있어야 함 -> minheap, maxheap 모두 두고 
+    # dict으로 개수를 관리하고 있자 (판단 기준)
+    maxheap, minheap = [], []
+    heapify(maxheap) # -로 저장
+    heapify(minheap) # 그대로 저장
+    count = defaultdict(int)
     
-    for i, operation in enumerate(operations):
-        if operation[0] == "I":
-            num = int(operation[2:])
-            heappush(minheap, (num, i))
-            heappush(maxheap, (-num, i))
+    operations = deque(operations)
+    
+    while operations:
+        command, num = operations.popleft().split()
+        num = int(num)
+        
+        if command == 'I':
+            heappush(maxheap, -num)
+            heappush(minheap, num)
+            count[num] += 1
         else:
-            if operation[2] == '-': # 최솟값 삭제
-                while minheap:
-                    x, idx = heappop(minheap)
-                    if idx not in visited:
-                        visited.add(idx)
-                        break
-            else: # 최댓값 삭제
-                while maxheap:
-                    x, idx = heappop(maxheap)
-                    if idx not in visited:
-                        visited.add(idx)
-                        break
-    max_value, min_value = 0, 0
-    while maxheap:
-        x, idx = heappop(maxheap)
-        if idx not in visited:
-            max_value = -x
-            break
-    while minheap:
-        x, idx = heappop(minheap)
-        if idx not in visited:
-            min_value = x
-            break
+            if num == 1: # 최댓값 삭제
+                while maxheap and count[-maxheap[0]] == 0:
+                    heappop(maxheap)
+                if maxheap: 
+                    k = -heappop(maxheap)
+                    count[k] -= 1
+            else: # 최솟값 삭제
+                while minheap and count[minheap[0]] == 0:
+                    heappop(minheap)
+                if minheap:
+                    k = heappop(minheap)
+                    count[k] -= 1
+    final_min_heap, final_max_heap = [], []
+    heapify(final_max_heap)
+    heapify(final_min_heap)
     
-    return [max_value, min_value]
+    for a in count:
+        for _ in range(count[a]):
+            heappush(final_max_heap, -a)
+            heappush(final_min_heap, a)
+    
+    return [0, 0] if not final_max_heap else [-heappop(final_max_heap), heappop(final_min_heap)]
+            
