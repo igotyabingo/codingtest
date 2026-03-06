45e1359a51bfa3b69d2944dfdd825b65ab17d8c7
igotyabingo <jenifer7933@snu.ac.kr>
2026-03-06 19:06:37 +0900

    Refactor H-Index calculation logic

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,8 @@
 def solution(citations):
     citations.sort(reverse=True)
+    h = 0
+    
     for i, val in enumerate(citations):
-        if i+1 > val:
-            return i
-    return len(citations)
+        h = max(h, min(i+1, val))
+    
+    return h
